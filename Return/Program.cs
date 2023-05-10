85d98b007c391081accf2a87c8016ddaf9b42b85
Marcelo Cesar <marcelocesar9812@gmail.com>
2023-05-10 15:16:49 -0300

    first commit

---
+++ Return/Program.cs
@@ -0,0 +1,15 @@
+﻿string funcao(int n)
+{
+    if (n > 0 && n % 2 == 0)
+        return "POSITIVO E PAR";
+    if (n > 0 && n % 2 == 1)
+        return "POSITIVO E IMPAR";
+    if (n < 0 && n % 2 == 0)
+        return "NEGATIVO E PAR";
+    else
+        return "NEGATIVO E IMPAR";
+
+}
+
+Console.WriteLine(funcao(-10));
+
